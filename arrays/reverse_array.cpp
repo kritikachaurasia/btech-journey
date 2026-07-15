0f585adbefa223f5e1e6f9aab6c7e7e9a212464c
kritikachaurasia <kritikachaurasia471@gmail.com>
2026-07-15 08:21:28 +0530

    Day 4: Array- Reverse Array

---
+++ arrays/reverse_array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void reverseArray( int arr[], int sz){
+int start=0, end=sz-1;
+while(start<end){
+    swap(arr[start], arr[end]);
+    start++;
+    end--;
+}
+}
+int main(){
+    int arr[]={4,2,7,8};
+    int sz=4;
+    reverseArray(arr,sz);
+    for(int i=0; i<sz; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+    
+
+
