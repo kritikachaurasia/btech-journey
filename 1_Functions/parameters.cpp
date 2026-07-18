e70a5a22275b9447fa02bf93f46b6f19ccc4f424
kritikachaurasia <kritikachaurasia471@gmail.com>
2026-07-18 17:31:25 +0530

    practise more questions

--- 1_Functions/parameters.cpp
+++ 1_Functions/parameters.cpp
@@ -1,5 +1,5 @@
     // PARAMETERS:--
-    
+
     // returnType nameOfFunction ( type1 name1 , type2 name2)
 
     #include<iostream>
@@ -42,3 +42,54 @@ int minOfTwo(int a , int b){
 
         return 0;
     }
+
+
+
+    //SUM OF NO. FROM 1 TO N
+
+#include<iostream>
+using namespace std;
+
+int sumOfNumber(int n){
+    
+    int sum=0;
+    int count=1;
+    for(int i=0; i<n; i++){
+        sum+=count;
+        count+=1;
+    }
+    return sum;
+}
+
+int main(){
+    
+    cout<<"sum of numbers= "<< sumOfNumber(5)<<endl;
+    return 0;
+
+}
+
+
+
+
+//FACTORIAL OF N
+
+#include<iostream>
+using namespace std;
+
+int factorialOfN(int n){
+int factorial=1;
+int count =1 ;
+    for(int i=1; i<=n ; i++){
+        factorial= factorial * i;
+        count= count + 1;
+        
+
+    }
+    return factorial;
+}
+int main(){
+    cout<< "factorial of n = "<< factorialOfN(5)<<endl;
+    return 0;
+}
+
+
